b9b123ef95339ef14ec77ac753c8b8355c7e7f63
simonfals <156264734+simonfals@users.noreply.github.com>
2024-01-18 21:22:13 -0500

    Rock_Paper_Scissors.py

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -20,19 +20,19 @@ scissors = '''    _______
 ---.__(___)'''
 game = ([rock, paper, scissors])
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for paper and 2 for scissors.\n"))
-if user_choice > 2:
+if user_choice >= 3 or user_choice < 0:
     print("choose a number from 0-2\n You Lose")
-print(game[user_choice])
-computer_choice = (random.randint(0,2))
-com = game[computer_choice]
-if user_choice > computer_choice:
-    print(f"computer choose {com}\n YOU WIN")
-elif user_choice ==0 and computer_choice == 2:
-    print(f" computer choose {com}\n YOU WIN")
-elif user_choice == computer_choice:
-    print(f'computer choose {com}\n It\'s a draw')
 else:
-    print(f"computer choose {com}\n YOU LOSE")
-
-
-
+    print(game[user_choice])
+    if user_choice >= 3 or user_choice < 0:
+        print("choose a number from 0-2\n You Lose")
+    computer_choice = (random.randint(0,2))
+    com = game[computer_choice]
+    if user_choice > computer_choice:
+        print(f"computer choose {com}\n YOU WIN")
+    elif user_choice ==0 and computer_choice == 2:
+        print(f" computer choose {com}\n YOU WIN")
+    elif user_choice == computer_choice:
+        print(f'computer choose {com}\n It\'s a draw')
+    else:
+        print(f"computer choose {com}\n YOU LOSE")
