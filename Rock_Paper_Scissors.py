0abf757706f1c5012697ac0615c05dfe746944d7
simonfals <156264734+simonfals@users.noreply.github.com>
2024-01-18 21:07:40 -0500

    Add files via upload

---
+++ Rock_Paper_Scissors.py
@@ -0,0 +1,38 @@
+import random
+
+rock = '''    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)'''
+paper = '''    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)'''
+scissors = '''    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)'''
+game = ([rock, paper, scissors])
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for paper and 2 for scissors.\n"))
+if user_choice > 2:
+    print("choose a number from 0-2\n You Lose")
+print(game[user_choice])
+computer_choice = (random.randint(0,2))
+com = game[computer_choice]
+if user_choice > computer_choice:
+    print(f"computer choose {com}\n YOU WIN")
+elif user_choice ==0 and computer_choice == 2:
+    print(f" computer choose {com}\n YOU WIN")
+elif user_choice == computer_choice:
+    print(f'computer choose {com}\n It\'s a draw')
+else:
+    print(f"computer choose {com}\n YOU LOSE")
+
+
+
